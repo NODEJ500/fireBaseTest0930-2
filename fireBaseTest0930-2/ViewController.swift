346721acbf0fb047a30fb9b392b9de21928f1988
onodera <cxiaoyesi745@gmail.com>
2021-10-02 13:14:40 +0900

    initial Commit

--- fireBaseTest0930-2/ViewController.swift
+++ fireBaseTest0930-2/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
@@ -14,14 +15,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var pass: UITextField!
     
+    var changed: AuthStateDidChangeListenerHandle?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        FirebaseApp.configure()
-        label.text = FirebaseApp.app()?.name
+        
+        label.text = "emial & pass."
+        pass.isSecureTextEntry = true
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        changed = Auth.auth().addStateDidChangeListener {
+                 (auth, user) in
+            self.label.text = user?.email
+        }
+    }
    
-
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        Auth.auth().removeStateDidChangeListener(changed!)
+    }
+    
+    @IBAction func doAction(_ sender: Any) {
+        
+        let email = self.email.text
+        let password = self.pass.text
+        Auth.auth().signIn(withEmail: email ?? "", password: password ?? "") {
+            (user, error) in
+            if error != nil {
+                self.label.text = error?.localizedDescription
+                return
+            }
+        }
+    }
+    
 }
 
