bd11b19d174d0d3d040cb0b762aab8c1f3b48bd2
onodera <cxiaoyesi745@gmail.com>
2021-10-03 01:11:16 +0900

    initial Commit

--- fireBaseTest0930-2/ViewController.swift
+++ fireBaseTest0930-2/ViewController.swift
@@ -11,9 +11,8 @@ import Firebase
 
 class ViewController: UIViewController {
 
-  
-    @IBOutlet weak var msg: UITextField!
-    
+   
+    @IBOutlet weak var data: UITextView!
     var ref: DatabaseReference!
     
    
@@ -21,10 +20,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         ref = Database.database().reference()
+        
+        ref.child("people").observe(DataEventType.value,with: {(snapshot) in
+            var res = ""
+            if let values = snapshot.value as? NSArray {
+                for val in values {
+                    let ob:NSDictionary! = val as! NSDictionary
+                    let nm:String = ob.value(forKey: "name") as! String
+                    let ml:String = ob.value(forKey: "mail") as! String
+                    let ag:Int = ob.value(forKey: "age") as! Int
+                    res += nm + "[" + ml + ":" + String(ag) + "]\n"
+                }
+            }
+            self.data.text = res
+        }) { (error) in
+        print(error.localizedDescription)
     }
-    @IBAction func doAction(_ sender: Any) {
-        var msg = self.msg.text
-        ref.child("massage").setValue(msg)
-    }
+  }
 }
-
