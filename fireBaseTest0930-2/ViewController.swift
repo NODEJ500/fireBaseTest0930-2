19411de542a6d0c8d39070804a659f88884c3bc1
onodera <cxiaoyesi745@gmail.com>
2021-10-07 02:03:17 +0900

    initial Commit

--- fireBaseTest0930-2/ViewController.swift
+++ fireBaseTest0930-2/ViewController.swift
@@ -18,39 +18,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var age: UITextField!
     @IBOutlet weak var data: UITextView!
     
-    var db:Firestore!
-    var people: CollectionReference!
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let storage = Storage.storage()
-        
-        let txtRef = storage.reference(withPath: "sample.text")
-        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
-        let fileUrl = path.appendingPathComponent("sample.text")
-        txtRef.write(toFile:fileUrl) { url, error in
-            if let err = error {
-                self.data.text = err.localizedDescription
-            }else{
-                self.data.text = fileUrl.absoluteString
-            }
         }
-    }
-       
-        
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-    }
     
     @IBAction func doAction(_ sender: Any) {
        
-    
+        let storage = Storage.storage()
+        let txtRef = storage.reference(withPath: "newfile.text")
+        let str = data.text  + ""
+        txtRef.putData(str.data(using: String.Encoding.utf8)!, metadata:nil) { metadata, error in
+            if let meta = metadata {
+               print("成功です！")
+            }else{
+               print("失敗です！")
+         }
+        }
     }
 }
